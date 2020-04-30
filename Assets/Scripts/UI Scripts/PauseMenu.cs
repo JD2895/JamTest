c3f896092ca1b276ffae3cd208e731134656d17f
JD2895 <j.davis.2895@gmail.com>
2020-04-29 22:13:38 -0400

    added basic pause menu to my scene

---
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //Time.timeScale = 1f;
+        Debug.Log("TODO: Make a menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("TODO: Make an actual quit function (that will work in the editor and out of it)");
+        Application.Quit();
+    }
+
+    public void LoadDebugMenu()
+    {
+        Debug.Log("TODO: Make a debug menu");
+    }
+}
